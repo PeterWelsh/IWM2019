9a1e8f0d5215c21ded73b5bb8c26d542f7a89c9a
PeterWelsh <pwelsh1998@gmail.com>
2019-12-16 19:25:57 +0000

    Fixed my parallax following player

--- Assets/Scripts/ScrollingParallax.cs
+++ Assets/Scripts/ScrollingParallax.cs
@@ -6,35 +6,45 @@ public class ScrollingParallax : MonoBehaviour
 {
     private float length, startPos;
     public GameObject cam;
-    public float camSpeed = 2.0f;
-    float camPosX = 0;
+    public GameObject plane;
     public float parallaxEffect;
+    public float camSpeed;
+    float camPosX = 0;
+    Vector3 camStartPos, planePos;
+
+    private void Awake()
+    {
+        camStartPos = Camera.main.transform.position;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position.x;
-        camPosX = Camera.main.transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
+        planePos = plane.transform.position;
+
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
+
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
-        
-        if(temp > startPos + length)
+
+        if (temp > startPos + length)
         {
             startPos += length;
         }
-        else if(temp < startPos - length)
+        else if (temp < startPos - length)
         {
             startPos -= length;
         }
 
-        camPosX += camSpeed * Time.deltaTime;
-        cam.transform.position = new Vector3(camPosX, cam.transform.position.y, cam.transform.position.z);
+        camPosX += (camSpeed * Time.deltaTime);
+        cam.transform.position = new Vector3(camStartPos.x + planePos.x, cam.transform.position.y, cam.transform.position.z);
+        //cam.transform.position = new Vector3(startPos + plane.transform.position.x, 0.0f, 0.0f);
+        //cam.transform.Translate(new Vector3((7.0f * (parallaxEffect * Time.deltaTime)), 0.0f, 0.0f));
     }
 }
