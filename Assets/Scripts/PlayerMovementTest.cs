1f739637397f97fea3b517280fdb1fdc7b66b088
PeterWelsh <pwelsh1998@gmail.com>
2019-12-16 12:33:09 +0000

    Infinite parallax scrolling added

---
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTest : MonoBehaviour
+{
+    public float speed = 5.0f;
+    float posX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Moving left");
+            posX += speed * Time.deltaTime;
+        }
+
+        transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+    }
+}
